81564b11396dfd1f2bf07a5e60339ff2f48e7921
Chris Murphy <chris@strandz.org>
2017-06-19 07:51:44 +0800

    keywords and use of name-of

--- clojure/src/gilded_rose/refactored.clj
+++ clojure/src/gilded_rose/refactored.clj
@@ -1,30 +1,48 @@
 (ns gilded-rose.refactored
   (:require [gilded-rose.core :as orig]))
 
+(def kw->name {:backstage "Backstage passes to a TAFKAL80ETC concert"
+               :dexterity "+5 Dexterity Vest"
+               :elixir    "Elixir of the Mongoose"
+               :brie      "Aged Brie"
+               :sulfuras  "Sulfuras, Hand of Ragnaros"})
+
+(defn kw->item [kw]
+  (first (filter #(= (:name %) (kw kw->name)) orig/start-inventory)))
+
+(def items {:backstage (kw->item :backstage)
+            :dexterity (kw->item :dexterity)
+            :elixir    (kw->item :elixir)
+            :brie      (kw->item :brie)
+            :sulfuras  (kw->item :sulfuras)})
+
+(defn name-of [kw]
+  (-> items kw :name))
+
 (defn update-quality-new [items]
   (map
     (fn[item] (cond
-                (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
+                (and (< (:sell-in item) 0) (= (:name item) (name-of :backstage)))
                 (merge item {:quality 0})
-                (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-                (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
+                (or (= (:name item) (name-of :brie)) (= (:name item) (name-of :backstage)))
+                (if (and (= (:name item) (name-of :backstage)) (>= (:sell-in item) 5) (< (:sell-in item) 10))
                   (merge item {:quality (inc (inc (:quality item)))})
-                  (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
+                  (if (and (= (:name item) (name-of :backstage)) (>= (:sell-in item) 0) (< (:sell-in item) 5))
                     (merge item {:quality (inc (inc (inc (:quality item))))})
                     (if (< (:quality item) 50)
                       (merge item {:quality (inc (:quality item))})
                       item)))
                 (< (:sell-in item) 0)
-                (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
+                (if (= (name-of :backstage) (:name item))
                   (merge item {:quality 0})
-                  (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+                  (if (or (= (name-of :dexterity) (:name item)) (= (name-of :elixir) (:name item)))
                     (merge item {:quality (- (:quality item) 2)})
                     item))
-                (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+                (or (= (name-of :dexterity) (:name item)) (= (name-of :elixir) (:name item)))
                 (merge item {:quality (dec (:quality item))})
                 :else item))
     (map (fn [item]
-           (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
+           (if (not= (name-of :sulfuras) (:name item))
              (merge item {:sell-in (dec (:sell-in item))})
              item))
          items)))
