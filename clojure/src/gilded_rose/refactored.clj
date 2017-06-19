09b486c7dbc15f0f8ff767b167db2bf656bbc489
Chris Murphy <chris@strandz.org>
2017-06-19 08:11:58 +0800

    sulfuras map into big map, and now sep fn

--- clojure/src/gilded_rose/refactored.clj
+++ clojure/src/gilded_rose/refactored.clj
@@ -28,44 +28,44 @@
 (def zero-quality (u/zero-kw :quality))
 (def dec-sell-in (u/dec-kw :sell-in))
 
+(defn transition-quality [itm]
+  (if (= (name-of :sulfuras) (:name itm))
+    itm
+    (let [item (dec-sell-in itm 1)]
+      (cond
+        (and (< (:sell-in item) 0) (= (:name item) (name-of :backstage)))
+        (zero-quality item)
+
+        (appreciating (:name item))
+        (cond
+          (and (= (:name item) (name-of :backstage)) (>= (:sell-in item) 5) (< (:sell-in item) 10))
+          (inc-quality item 2)
+
+          (and (= (:name item) (name-of :backstage)) (>= (:sell-in item) 0) (< (:sell-in item) 5))
+          (inc-quality item 3)
+
+          (< (:quality item) 50)
+          (inc-quality item 1)
+
+          :else item)
+
+        (< (:sell-in item) 0)
+        (cond
+          (= (name-of :backstage) (:name item))
+          (zero-quality item)
+
+          (depreciating (:name item))
+          (dec-quality item 2)
+
+          :else item)
+
+        (depreciating (:name item))
+        (dec-quality item 1)
+
+        :else item))))
+
 (defn update-quality-new [items]
-  (map
-    (fn[item] (cond
-                (and (< (:sell-in item) 0) (= (:name item) (name-of :backstage)))
-                (zero-quality item)
-
-                (appreciating (:name item))
-                (cond
-                  (and (= (:name item) (name-of :backstage)) (>= (:sell-in item) 5) (< (:sell-in item) 10))
-                  (inc-quality item 2)
-
-                  (and (= (:name item) (name-of :backstage)) (>= (:sell-in item) 0) (< (:sell-in item) 5))
-                  (inc-quality item 3)
-
-                  (< (:quality item) 50)
-                  (inc-quality item 1)
-
-                  :else item)
-
-                (< (:sell-in item) 0)
-                (cond
-                  (= (name-of :backstage) (:name item))
-                  (zero-quality item)
-
-                  (depreciating (:name item))
-                  (dec-quality item 2)
-
-                  :else item)
-
-                (depreciating (:name item))
-                (dec-quality item 1)
-
-                :else item))
-    (map (fn [item]
-           (if (not= (name-of :sulfuras) (:name item))
-             (dec-sell-in item 1)
-             item))
-         items)))
+  (map transition-quality items))
 
 ;;
 ;; Switch on and off, depending whether want to to use the original or my refactored update-quality function
